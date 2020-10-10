97a2d7cda50136027addab0ae120190590f5325d
zhengkai13 <zhengkai1@jd.com>
2020-10-10 17:26:24 +0800

    增加注释

--- vue-src/platforms/web/runtime/index.js
+++ vue-src/platforms/web/runtime/index.js
@@ -7,12 +7,12 @@ import { mountComponent } from 'core/instance/lifecycle'
 import { devtools, inBrowser, isChrome } from 'core/util/index'
 
 import {
-  query,
-  mustUseProp,
-  isReservedTag,
-  isReservedAttr,
-  getTagNamespace,
-  isUnknownElement
+    query,
+    mustUseProp,
+    isReservedTag,
+    isReservedAttr,
+    getTagNamespace,
+    isUnknownElement
 } from 'web/util/index'
 
 import { patch } from './patch'
@@ -35,38 +35,38 @@ Vue.prototype.__patch__ = inBrowser ? patch : noop
 
 // public mount method
 /*组件挂载方法*/
-Vue.prototype.$mount = function (
-  el?: string | Element,
-  hydrating?: boolean
+Vue.prototype.$mount = function(
+    el ? : string | Element,
+    hydrating ? : boolean
 ): Component {
-  /*获取DOM实例对象*/
-  el = el && inBrowser ? query(el) : undefined
-  /*挂载组件*/
-  return mountComponent(this, el, hydrating)
+    /*获取DOM实例对象*/
+    el = el && inBrowser ? query(el) : undefined
+        /*挂载组件*/
+    return mountComponent(this, el, hydrating)
 }
 
 // devtools global hook
 /* istanbul ignore next */
 setTimeout(() => {
-  if (config.devtools) {
-    if (devtools) {
-      devtools.emit('init', Vue)
-    } else if (process.env.NODE_ENV !== 'production' && isChrome) {
-      console[console.info ? 'info' : 'log'](
-        'Download the Vue Devtools extension for a better development experience:\n' +
-        'https://github.com/vuejs/vue-devtools'
-      )
+    if (config.devtools) {
+        if (devtools) {
+            devtools.emit('init', Vue)
+        } else if (process.env.NODE_ENV !== 'production' && isChrome) {
+            console[console.info ? 'info' : 'log'](
+                'Download the Vue Devtools extension for a better development experience:\n' +
+                'https://github.com/vuejs/vue-devtools'
+            )
+        }
     }
-  }
-  if (process.env.NODE_ENV !== 'production' &&
-      config.productionTip !== false &&
-      inBrowser && typeof console !== 'undefined') {
-    console[console.info ? 'info' : 'log'](
-      `You are running Vue in development mode.\n` +
-      `Make sure to turn on production mode when deploying for production.\n` +
-      `See more tips at https://vuejs.org/guide/deployment.html`
-    )
-  }
+    if (process.env.NODE_ENV !== 'production' &&
+        config.productionTip !== false &&
+        inBrowser && typeof console !== 'undefined') {
+        console[console.info ? 'info' : 'log'](
+            `You are running Vue in development mode.\n` +
+            `Make sure to turn on production mode when deploying for production.\n` +
+            `See more tips at https://vuejs.org/guide/deployment.html`
+        )
+    }
 }, 0)
 
 export default Vue
